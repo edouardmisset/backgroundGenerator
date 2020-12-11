a2ea5e190f6da676407ad35a80fb05c64d389d65
Edouard Misset <edouardmisset@gmail.com>
2020-12-11 13:16:02 +0100

    Simplifications

--- script.js
+++ script.js
@@ -2,59 +2,20 @@ var backgroundCSS = "",
   destination = document.querySelector("code"),
   body = document.querySelector("body"),
   colorInputs = document.querySelectorAll("input"),
-  firstColor = document.getElementById("firstColor").value,
-  secondColor = document.getElementById("secondColor").value;
+  firstColor = document.getElementById("firstColor"),
+  secondColor = document.getElementById("secondColor");
 
-// Add event listeners for "input" and "change" events to the inputs
-function addingListeners() {
-  colorInputs.forEach((element) => {
-    element.addEventListener("input", update);
-    element.addEventListener("change", update);
-  });
-}
+// Adding two event listeners of type input to the color pickers.
+firstColor.addEventListener("input", update);
+secondColor.addEventListener("input", update);
 
-addingListeners();
 displayCSSBackground();
 
-/**
- * @description Main function : set the background colors and displays the CSS code
- * @date 11/12/2020
- * @param {*} event
- * @return {*}
- */
-function update(event) {
-  // If the first/secind input is selected then set the first/second color. else nothing
-  // TODO: simplify. Not very elegant...
-  if (event.target === colorInputs[0]) {
-    firstColor = event.target.value;
-  } else if (event.target === colorInputs[1]) {
-    secondColor = event.target.value;
-  } else {
-    return 0;
-  }
-
-  displayCSSBackground();
-  setBackgroundColors();
-}
-
-/**
- * @description Sets the CSS code for the backgroun in the paragraph
- * @date 11/12/2020
- */
-function displayCSSBackground() {
-  buildCSSString();
+// Main function to create the string of CSS code and call the other 2 functions
+function update() {
+  backgroundCSS = `linear-gradient(to bottom right, ${firstColor.value},${secondColor.value})`;
+  // displays the CSS code of the background
   destination.textContent = backgroundCSS;
-}
-
-/**
- * @description Builds the string of the CSS background property
- * @date 11/12/2020
- */
-function buildCSSString() {
-  backgroundCSS = `linear-gradient(to bottom right, ${firstColor},${secondColor})`;
-}
-
-// Sets the CSS property "background" = linear-gradient(to bottom right, firstColor, secondColor)
-function setBackgroundColors() {
-  body.style.background = buildCSSString();
+  // Sets the CSS  of the body's background
+  body.style.background = backgroundCSS;
 }
